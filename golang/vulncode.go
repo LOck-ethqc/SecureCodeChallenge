1f2377b3a9adb8e6836fa2a0b4a8dbefd628c68d
LOck-thqc <lock.ethqc@gmail.com>
2025-01-09 09:34:57 -0500

    Newest: LFI(Weak path sanitization)

---
+++ golang/vulncode.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "path/filepath"
+    "strings"
+)
+
+func main() {
+    http.HandleFunc("/download", downloadHandler)
+    fmt.Println("Server started at :8080")
+    http.ListenAndServe(":8080", nil)
+}
+
+func downloadHandler(w http.ResponseWriter, r *http.Request) {
+    // Gets the query parameter value
+    filename := r.URL.Query().Get("filename")
+    // Checks if the query parameter value was empty(No filename requested)
+    if filename == "" {
+        http.Error(w, "Missing filename parameter", http.StatusBadRequest)
+        return
+    }
+    fmt.Println("Query request:", filename) 
+
+    // Preventing Path Traversal
+    // -1 means that it ensures that it replaces all occurances of "../"
+    sanitizedFilename := filename = strings.Replace(filename, "../", "", -1)
+    cleanedFilename := filepath.Clean(sanitizedFilename)
+    baseDir := "public/files/"
+    fullPath := filepath.Join(baseDir, cleanedFilename)
+    // /public/files/<The requested file name>
+    fmt.Println("Query Request (POST-sanitization):", fullPath)
+
+    w.Header().Set("Content-Disposition", fmt.Sprintf("inline; filename=\"%s\"", filepath.Base(fullPath)))
+    w.Header().Set("Content-Type", "text/plain")
+    http.ServeFile(w, r, fullPath)
+}
+
